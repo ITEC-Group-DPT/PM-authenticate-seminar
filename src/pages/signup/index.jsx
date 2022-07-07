4253a0fac9e724633ee3862d579ca932e6bed9f8
JustTory <johnnynathany@gmail.com>
2022-07-07 20:01:25 +0700

    add validation for all fields

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,14 +1,82 @@
 import React, { useState } from 'react';
-import { LockClosedIcon } from '@heroicons/react/solid';
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [phone, setPhone] = useState('');
-  const [password1, setPassword1] = useState('');
-  const [password2, setPassword2] = useState('');
+  const [email, setEmail] = useState({
+    value: '',
+    error: '',
+  });
+  const [username, setUsername] = useState({
+    value: '',
+    error: '',
+  });
+  const [phone, setPhone] = useState({
+    value: '',
+    error: '',
+  });
+  const [password1, setPassword1] = useState({
+    value: '',
+    error: '',
+  });
+  const [password2, setPassword2] = useState({
+    value: '',
+    error: '',
+  });
 
-  console.log(email);
+  const emailRegex = /^[a-zA-Z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+  const usernameRegex = /^[a-zA-Z0-9]{6,20}$/;
+  const phoneRegex = /^[0-9]{10}$/;
+  const passwordRegex = /^[a-zA-Z0-9]{6,25}$/;
+
+  const isValid = (inp, regex) => {
+    if (inp.match(regex)) return true;
+    return false;
+  };
+
+  const isExist = (inp, type) => {
+    const users = JSON.parse(localStorage.getItem('users'));
+    return users.some((user) => user[type] === inp);
+  };
+
+  const validation = (emailInp, usernameInp, phoneInp, pw1, pw2) => {
+    // email
+    if (isValid(emailInp, emailRegex))
+      if (isExist(emailInp, 'email')) setEmail({ ...email, error: 'Email is already taken' });
+      else setEmail({ ...email, error: '' });
+    else setEmail({ ...email, error: 'Email is invalid' });
+
+    // username
+    if (isValid(usernameInp, usernameRegex))
+      if (isExist(usernameInp, 'username'))
+        setUsername({ ...username, error: 'Username is already taken' });
+      else setUsername({ ...username, error: '' });
+    else
+      setUsername({
+        ...username,
+        error: 'Username must be between 6-20 length with no special character',
+      });
+
+    // phone
+    if (!isValid(phoneInp, phoneRegex))
+      setPhone({ ...phone, error: 'Phone number must be 10-digits' });
+    else setPhone({ ...phone, error: '' });
+
+    // password1
+    if (!isValid(pw1, passwordRegex))
+      setPassword1({
+        ...password1,
+        error: 'Password must be between 6-25 length with no special character',
+      });
+    else setPassword1({ ...password1, error: '' });
+
+    // password2
+    if (pw2 !== pw1) setPassword2({ ...password2, error: "Passwords don't match" });
+    else setPassword2({ ...password2, error: '' });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validation(email.value, username.value, phone.value, password1.value, password2.value);
+  };
 
   return (
     <div className="h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -37,11 +105,12 @@ const SignUp = () => {
                 className="input-form"
                 placeholder="Email address"
                 required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                value={email.value}
+                onChange={(e) => setEmail({ ...email, value: e.target.value })}
               />
             </div>
-            <p className="content-none">error</p>
+            <p className="error-msg">{email.error}</p>
+
             <div>
               <label htmlFor="username" className="sr-only">
                 Username
@@ -54,11 +123,12 @@ const SignUp = () => {
                 className="input-form"
                 placeholder="Username"
                 required
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                value={username.value}
+                onChange={(e) => setUsername({ ...username, value: e.target.value })}
               />
             </div>
-            <p>error</p>
+            <p className="error-msg">{username.error}</p>
+
             <div>
               <label htmlFor="phone" className="sr-only">
                 Phone
@@ -71,11 +141,12 @@ const SignUp = () => {
                 className="input-form"
                 placeholder="Phone number"
                 required
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
+                value={phone.value}
+                onChange={(e) => setPhone({ ...phone, value: e.target.value })}
               />
             </div>
-            <p>error</p>
+            <p className="error-msg">{phone.error}</p>
+
             <div>
               <label htmlFor="password" className="sr-only">
                 Password
@@ -88,11 +159,12 @@ const SignUp = () => {
                 className="input-form"
                 placeholder="Password"
                 required
-                value={password1}
-                onChange={(e) => setPassword1(e.target.value)}
+                value={password1.value}
+                onChange={(e) => setPassword1({ ...password1, value: e.target.value })}
               />
             </div>
-            <p>error</p>
+            <p className="error-msg">{password1.error}</p>
+
             <div>
               <label htmlFor="password" className="sr-only">
                 Confirm Password
@@ -105,20 +177,14 @@ const SignUp = () => {
                 className="input-form"
                 placeholder="Confirm password"
                 required
-                value={password2}
-                onChange={(e) => setPassword2(e.target.value)}
+                value={password2.value}
+                onChange={(e) => setPassword2({ ...password2, value: e.target.value })}
               />
             </div>
-            <p>error</p>
+            <p className="error-msg">{password2.error}</p>
           </div>
           <div>
-            <button type="submit" className="submit-btn">
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <LockClosedIcon
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                  aria-hidden="true"
-                />
-              </span>
+            <button className="submit-btn" type="submit" onClick={(e) => handleSubmit(e)}>
               Sign up
             </button>
           </div>
