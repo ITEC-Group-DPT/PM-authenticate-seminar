3c5b75460a09091acbedd1d4b30aa808d1a1c6c1
tranngminhdao <107382512+tranngminhdao@users.noreply.github.com>
2022-07-08 23:47:43 +0700

    Done signin function and test cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import SignIn from 'pages/signin';
 import SignUp from 'pages/signup';
 import Home from 'pages/home';
+import userData from 'users.json';
 
 const AppRoutes = () => {
+  useEffect(() => {
+    localStorage.setItem('users', JSON.stringify(userData));
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
