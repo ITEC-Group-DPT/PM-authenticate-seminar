5af32e4606e8f3f8eb2cac2312ae67dddfecf3e4
Phu Truong <tezukashuko@gmail.com>
2022-07-24 21:49:49 +0700

    test sign in

--- src/pages/signin/index.jsx
+++ src/pages/signin/index.jsx
@@ -72,7 +72,7 @@ const SignIn = ({ onTestSubmit }) => {
             alt="Workflow"
           />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+            asdasd
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
